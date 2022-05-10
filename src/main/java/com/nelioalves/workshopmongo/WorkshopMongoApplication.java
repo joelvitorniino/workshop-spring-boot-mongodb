c5d5d883e028e297d5ae1453f4e9762cc6649702
= <joelprofessor13@gmail.com>
2022-05-10 17:00:18 -0300

    Correcting to Camel Case

--- src/main/java/com/nelioalves/workshopmongo/WorkshopMongoApplication.java
+++ src/main/java/com/nelioalves/workshopmongo/WorkshopMongoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WorkshopmongoApplication {
+public class WorkshopMongoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WorkshopmongoApplication.class, args);
+		SpringApplication.run(WorkshopMongoApplication.class, args);
 	}
 
 }
