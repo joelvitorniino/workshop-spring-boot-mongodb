fa136a6f7bc36ee76779dad50b1947dc0458a186
= <joelprofessor13@gmail.com>
2022-05-15 14:30:18 -0300

    Using pattern DTO to return users

---
+++ src/main/java/com/nelioalves/workshopmongo/dto/UserDTO.java
@@ -0,0 +1,44 @@
+package com.nelioalves.workshopmongo.dto;
+
+import com.nelioalves.workshopmongo.domain.User;
+
+import java.io.Serializable;
+
+public class UserDTO implements Serializable {
+    private String id;
+    private String name;
+    private String email;
+
+    public UserDTO() {
+    }
+
+    public UserDTO(User obj) {
+        id = obj.getId();
+        name = obj.getName();
+        email = obj.getEmail();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
